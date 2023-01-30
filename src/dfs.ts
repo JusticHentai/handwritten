4b52ce2745107d3afe4facadadde462d2d5bc410
JusticHentai <1360636180@qq.com>
2023-01-30 16:55:39 +0800

    feat: color

--- src/dfs.ts
+++ src/dfs.ts
@@ -16,3 +16,28 @@ export const dfs1 = (array: any[]) => {
 
   return dfs(array)
 }
+
+export const dfs2 = (array: number[], target: number): number[][] => {
+  const res: number[][] = []
+
+  const dfs = (list: number[], index: number, sum: number) => {
+    if (sum === target) {
+      res.push(list)
+      return
+    }
+
+    for (let i = index; i < array.length; i++) {
+      if (sum + array[index] > target) {
+        continue
+      }
+
+      list.push(array[index])
+      dfs(list, index + 1, sum + array[index])
+      list.pop()
+    }
+  }
+
+  dfs(array, 0, 0)
+
+  return res
+}
--- test/dfs.test.ts
+++ test/dfs.test.ts
@@ -1,7 +1,10 @@
-import { dfs1 } from '../src/dfs'
+import { dfs1, dfs2 } from '../src/dfs'
 
 test('测试 src/dfs.ts', () => {
   const arr = [1, [6, [10, [1, [1, [1]]]]], 2, 3, [4, 5, [6, 7, [8, 9, [10]]]]]
 
   expect(dfs1(arr)).toBe(6)
+
+  const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  console.log(dfs2(arr2, 10))
 })
