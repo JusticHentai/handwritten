c0c89d8d2781a7ea1bf269adaba0ff3d6c67bcd0
JusticHentai <1360636180@qq.com>
2024-08-13 18:09:45 +0800

    更新版本

--- test/flat.test.ts
+++
@@ -1,7 +0,0 @@
-import flat from '../src/flat'
-
-test('测试 src/flat.ts', () => {
-  const arr = [1, [[2, 3], 4], 5]
-
-  expect(flat(arr, Infinity)).toEqual([1, 2, 3, 4, 5])
-})
--- test/hash.test.ts
+++
@@ -1,7 +0,0 @@
-import { hash1 } from '../src/hash'
-
-test('测试 src/hash.ts', () => {
-  const arr = [8, 1, 2, 3, 4, 5, 6, 7]
-
-  expect(hash1(arr)).toBe(7)
-})
